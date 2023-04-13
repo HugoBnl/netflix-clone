4954cd952cf65ba6d09c20cb7c7608e929a5f198
HugoBnl <bonnelhugo@gmail.com>
2023-04-13 17:20:20 +0200

    Video Player

--- components/MovieCard.tsx
+++ components/MovieCard.tsx
@@ -1,5 +1,5 @@
 import react from 'react'
-
+import { useRouter } from 'next/router';
 import { BsFillPlayFill } from 'react-icons/bs'
 import FavoriteButton from './FavoritesButton';
 
@@ -10,6 +10,8 @@ interface MovieCardProps{
 const MovieCard: React.FC <MovieCardProps> = ({
     data
 }) => {
+
+    const router = useRouter();
     return (
         <div className='group bg-zinc-900 col-span relative h-[12vw]'>
             <img
@@ -28,7 +30,7 @@ const MovieCard: React.FC <MovieCardProps> = ({
                 <div className='z-10 bg-zinc-800 p-2 lg:p-4 absolute w-full transition shadow-md rounded-b-md'>
                     <div className=' flex flex-row items-center gap-3'>
                         <div className=' cursor-pointer w-6 h-6 lg:w-10 lg:h-10 bg-white rounded-full flex justify-center items-center transition hover:bg-neutral-300'
-                        onClick={()=>{}}>
+                        onClick={()=>router.push(`/watch/${data?.id}`)}>
                             <BsFillPlayFill size = {30} />
                         </div>
                         <FavoriteButton movieId={data?.id}/>
