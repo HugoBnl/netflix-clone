0feb018200762f2a71242a3463d9be17446c7ea2
HugoBnl <bonnelhugo@gmail.com>
2023-04-14 17:20:10 +0200

    add modal for info

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,10 @@
 import Billboard from "@/components/Billboard";
 import MovieList from "@/components/MovieList";
 import Navbar from "@/components/Navbar";
+import InfoModal from "@/components/infoModal";
 import useCurrentUser from "@/hooks/useCurrentUser";
 import useFavorites from "@/hooks/useFavorites";
+import useInfoModal from "@/hooks/useInfoModal";
 import useMovieList from "@/hooks/useMovieList";
 import { NextPageContext } from "next"
 import { getSession, signOut } from "next-auth/react"
@@ -28,16 +30,17 @@ export async function getServerSideProps(context: NextPageContext){
 export default function Home() {
   const { data: movies = [] } = useMovieList();
   const { data: favorites = []} =useFavorites();
+  const { isOpen, closeModal } = useInfoModal();
 
   return (
     <>
+     <InfoModal visible={isOpen} onClose={closeModal} />
      <Navbar />
      <Billboard />
      <div className="pb-40">
-     <MovieList title='Trending now'data={movies} />
-     <MovieList title='My Favorites'data={favorites} />
+      <MovieList title='Trending now'data={movies} />
+      <MovieList title='My Favorites'data={favorites} />
      </div>
-     
     </>
   )
 }
